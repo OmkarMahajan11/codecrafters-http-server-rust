a03ced70bdd395e90f15e198482adfbccb47a551
lenovo1 <OmkarMahajan11>
2026-05-28 15:11:36 +0530

    codecrafters submit [skip ci]

--- src/main.rs
+++ src/main.rs
@@ -14,8 +14,6 @@ fn main() {
     // You can use print statements as follows for debugging, they'll be visible when running tests.
     println!("Logs from your program will appear here!");
 
-    // TODO: Uncomment the code below to pass the first stage
-    //
     let listener = TcpListener::bind("127.0.0.1:4221").unwrap();
 
     for stream in listener.incoming() {
@@ -30,19 +28,31 @@ fn main() {
                 match parse_request(request_bytes) {
                     Ok(req) => {
                         println!("Parsed request: {:#?}", req);
-                    if req.path == "/" {
-                        _ = stream.write_all(b"HTTP/1.1 200 OK\r\n\r\n");
-                    } else if req.path.starts_with("/echo/") {
-                        let echo_str = &req.path["/echo/".len()..];
-                        let len = echo_str.len();
-                        let response = format!(
-                            "HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: {}\r\n\r\n{}",
-                            len, echo_str
-                        );
-                        _ = stream.write_all(response.as_bytes());
-                    } else {
-                        _ = stream.write_all(b"HTTP/1.1 404 Not Found\r\n\r\n");
-                    }
+                        if req.path == "/" {
+                            _ = stream.write_all(b"HTTP/1.1 200 OK\r\n\r\n");
+                        } else if req.path.starts_with("/echo/") {
+                            let echo_str = &req.path["/echo/".len()..];
+                            let len = echo_str.len();
+                            let response = format!(
+                                "HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: {}\r\n\r\n{}",
+                                len, echo_str
+                            );
+                            _ = stream.write_all(response.as_bytes());
+                        } else if req.path == "/user-agent" {
+                            match req.headers.get("User-Agent") {
+                                Some(agent) => {
+                                    let response = format!(
+                                        "HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: {}\r\n\r\n{}",
+                                        agent.len(),
+                                        agent
+                                    );
+                                    _ = stream.write_all(response.as_bytes());
+                                }
+                                None => _ = stream.write_all(b"HTTP/1.1 404 Not Found\r\n\r\n"),
+                            }
+                        } else {
+                            _ = stream.write_all(b"HTTP/1.1 404 Not Found\r\n\r\n");
+                        }
                     }
                     Err(e) => {
                         println!("Failed to parse request: {}", e);
